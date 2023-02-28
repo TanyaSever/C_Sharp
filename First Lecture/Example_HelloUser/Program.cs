8d677a082d520c1742fd5bb2fb3ff4836afb9708
TanyaSever <tanya13sever@gmail.com>
2023-02-28 15:40:48 +1000

    Sum

---
+++ First Lecture/Example_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Привет, как тебя зовут? ");
+string username = Console.ReadLine(); //оператор равенства значит возьми то что справа и положи в то что слева
+Console.Write("Привет, ");
+Console.Write(username);
---
+++ First Lecture/Example_Sum/Program.cs
@@ -0,0 +1,5 @@
+﻿int numbA = 3;
+int numbB = 4;
+Console.Write("У нас есть 2 числа");
+Console.WriteLine("Их сумма равна ");
+Console.Write(numbA + numbB);
