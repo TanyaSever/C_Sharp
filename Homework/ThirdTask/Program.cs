8b4ef97576d515bc8ed1ae409754d7db1316916b
TanyaSever <tanya13sever@gmail.com>
2023-03-01 22:26:28 +1000

    Homework.TaskThree

---
+++ Homework/ThirdTask/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введи любое число:  ");
+string numberA = Console.ReadLine()!;
+int x = int.Parse(numberA);
+if (x % 2 == 0) Console.WriteLine("Данное число является чётным");
+else Console.WriteLine("Это число нечётное");
