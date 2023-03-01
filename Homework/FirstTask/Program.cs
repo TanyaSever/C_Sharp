55629d9051d1dc690776a3ce8b816ecbcc488d6b
TanyaSever <tanya13sever@gmail.com>
2023-03-01 21:38:17 +1000

    Homework.TaskOne

---
+++ Homework/FirstTask/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введи любое число:  ");
+string numberA = Console.ReadLine()!;
+int a = int.Parse(numberA);
+Console.WriteLine("И еще одно введи:  ");
+string numberB = Console.ReadLine()!;
+int b = int.Parse(numberB);
+if(b<a)
+{
+    Console.WriteLine("Ну получается, что первое число больше");
+}
+else if(b>a)
+{
+    Console.WriteLine("Выходит, что второе число больше");
+}
+else if(b == a)
+{
+    Console.WriteLine("Никто не круче, расходимся");
+}
