0d4e3692808bb734f1723818df71697d58dcf4b1
TanyaSever <tanya13sever@gmail.com>
2023-03-01 20:57:51 +1000

    Second Task

--- First Seminar/FirstTask_Example/Program.cs
+++ First Seminar/FirstTask_Example/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Первое задание. Показывает преподователь
 Console.WriteLine("Введи любое число:  ");
-string numberA = Console.ReadLine()!;//успокаиваем программу с помощью !
-int num = int.Parse(numberA);
+string numberA = Console.ReadLine()!;//успокаиваем программу с помощью ! ReadLine считывает только строки
+int num = int.Parse(numberA);//преобразование в целочисленное значение из строкового
 Console.WriteLine("А это его квадрат: ");
 Console.Write(num*num);
---
+++ First Seminar/SecondTask/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введи любое число:  ");
+string numberA = Console.ReadLine()!;
+int a = int.Parse(numberA);
+Console.WriteLine("И еще одно введи:  ");
+string numberB = Console.ReadLine()!;
+int b = int.Parse(numberB);
+
+if(b*b == a)
+{
+    Console.WriteLine("a это b в квадрате!");
+}
+else if(a*a == b)
+{
+    Console.WriteLine("А тут все наоборот, b это a в квадрате");
+}
+else
+{
+    Console.WriteLine("Эти два числа рандомны");
+}
+
+//если писать console с маленькой буквы, то чудес не происходит
