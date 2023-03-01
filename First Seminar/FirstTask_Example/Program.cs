105dfbd7b4c7caa3a3228fbaec570313b4bd15c0
TanyaSever <tanya13sever@gmail.com>
2023-03-01 20:40:38 +1000

    FirstTasK

---
+++ First Seminar/FirstTask_Example/Program.cs
@@ -0,0 +1,6 @@
+﻿//Первое задание. Показывает преподователь
+Console.WriteLine("Введи любое число:  ");
+string numberA = Console.ReadLine()!;//успокаиваем программу с помощью !
+int num = int.Parse(numberA);
+Console.WriteLine("А это его квадрат: ");
+Console.Write(num*num);
