68352a03c76ea220d96fd7badb761153f518b445
TanyaSever <tanya13sever@gmail.com>
2023-03-01 18:13:02 +1000

    Based

---
+++ First Lecture/Example_Based/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 5;
+int b = 8;
+int c = 1;
+int d = 16;
+int e = 3;
+int max = a;
+if (a>max) max = a; 
+if (b>max) max = b; 
+if (c>max) max = c; 
+if (d>max) max = d; 
+if (e>max) max = e; 
+Console.Write("Максимальное значение это - ");
+Console.WriteLine(max);
