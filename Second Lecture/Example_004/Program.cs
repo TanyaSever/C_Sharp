01ce66f5adb848cc2711f719d6ae27af355af3bc
TanyaSever <tanya13sever@gmail.com>
2023-03-05 19:47:03 +1000

    The Hardest One

---
+++ Second Lecture/Example_004/Program.cs
@@ -0,0 +1,29 @@
+﻿//метод void - не возвращает ничего, тогда в коде нельзя использовать оператор result 
+int[] array = new int[10];
+void FillArray(int[] collection)
+{
+  int length = collection.Length; //определяем длину массива
+  int index = 0;
+  while (index < length)
+  {
+    collection[index] = new Random().Next(1, 10);
+    index++;
+  }
+}
+
+void PrintArray(int[] coll)
+{
+  int count = coll.Length;
+  int position = 0;
+  while (position < count)
+  {
+    Console.WriteLine(coll[position]);
+    position++;
+  }
+}
+//новый массив в котором по умолчанию 10 элементов
+FillArray(array);
+PrintArray(array);
+
+//определили массивчик из 10 элементов, затем вызвали метод FillArray который заполнил нам массив 
+//затем метод PrintArray нам его распечатал
