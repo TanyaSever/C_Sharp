51a5c2f3ace505046ecaa8172873b0318c74b8a2
TanyaSever <tanya13sever@gmail.com>
2023-03-06 00:30:17 +1000

    Multiplicity

--- Second Seminar/FirstTask_Example/Program.cs
+++ Second Seminar/FirstTask_Example/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//отображать посл цифру трехзначного числа
+Console.Write("У нас есть трехзначное число: ");
+int numbA = new Random().Next(100, 1000);
+//Console.Write("Последняя цифра этого числа: ");
+//Console.Write(numbA % 10); //% возвращает посл цифру от деления
+
+int TakeNum(int numbA)
+{
+  Console.WriteLine(numbA);
+  return numbA % 10;
+}
+
+int result = TakeNum(numbA);
+Console.Write("Последняя цифра этого числа: ");
+Console.Write(result);
